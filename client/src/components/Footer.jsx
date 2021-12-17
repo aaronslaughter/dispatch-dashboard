db505c2630a7ed243de95c89b5db189bf16472f2
Aaron Slaughter <aslaugh1@gmail.com>
2021-12-17 12:29:23 -0500

    set up the home page skeleton

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import GitHubMark from '../assets/GitHub-Mark-64px.png'
+import LinkedInLogo from '../assets/LI-In-Bug.png'
 
 export default function Footer() {
   return (
-    <div>
-      Footer Placeholder
-    </div>
+    <footer>
+      <a href="https://github.com/aaronslaughter/dispatch-dashboard" target="_blank" rel="noopener noreferrer"><img src={GitHubMark}/></a>
+      <a href="https://www.linkedin.com/in/aaron-slaughter1/" target="_blank" rel="noopener noreferrer"><img src={LinkedInLogo} style={{width:64 + 'px'}}/></a>
+    </footer>
   )
 }
