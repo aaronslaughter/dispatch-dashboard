ff4b9fcebbd4bde546ef940c38e0e0c80335158f
Aaron Slaughter <aslaugh1@gmail.com>
2021-12-20 14:49:55 -0500

    fixed typo in update technician controller

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -184,7 +184,7 @@ const updateTechnician = async (req, res) => {
         if (!technician) {
                 res.status(500).send('Technician not found!');
             }
-            return res.status(200).json(ticket);
+            return res.status(200).json(technician);
         })
     } catch (error) {
         return res.status(500).send(error.message);
