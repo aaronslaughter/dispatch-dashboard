7e9f12522cbfb47a598d30b543211526524e66f0
Aaron Slaughter <aslaugh1@gmail.com>
2021-12-21 14:08:15 -0500

    completed techs card functionality

--- client/src/components/dashboard/TechsCard.jsx
+++ client/src/components/dashboard/TechsCard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-export default function TechsCard() {
+export default function TechsCard(props) {
+
+  const [onSite, setOnSite] = useState(0)
+  const [enRoute, setEnRoute] = useState(0)
+  const [available, setAvailable] = useState(0)
+  const [offline, setOffline] = useState(0)
+
+  useEffect(() => {
+    setOnSite(props.technicians.filter((element) => element.status === 'On Site').length)
+    setEnRoute(props.technicians.filter((element) => element.status === 'En Route').length)
+    setAvailable(props.technicians.filter((element) => element.status === 'Available').length)
+    setOffline(props.technicians.filter((element) => element.status === 'Offline').length)
+  }, [props.tickets])
+
   return (
     <section>
-      Technician Card Placeholder
+      <p>Techs on site: {onSite}</p>
+      <p>Techs en route: {enRoute}</p>
+      <p>Techs available: {available}</p>
+      <p>Techs offline: {offline}</p>
     </section>
   )
 }
