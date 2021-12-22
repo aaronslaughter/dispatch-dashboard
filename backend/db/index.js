c219f90da2245badb4b28ae1dc15cf0da5c73035
Aaron Slaughter <aslaugh1@gmail.com>
2021-12-22 14:25:20 -0500

    heroku setup

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-let dbUrl = 'mongodb://127.0.0.1:27017/dispatch'
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/dispatch'
 
 mongoose
-    .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+.connect(dbUrl, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
     .then(() => {
         console.log('Successfully connected to MongoDB.')
     })
-    .catch(e => {
+    .catch((e) => {
         console.error('Connection error', e.message)
     })
+mongoose.set('debug', true)
 
 const db = mongoose.connection
 
